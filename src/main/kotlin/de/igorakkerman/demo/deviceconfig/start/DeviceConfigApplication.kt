434898b57bb3ae7ab1e688aa77f9da1706494428
Igor Akkerman <commit@dynamite.software>
2021-02-17 00:29:05 +0100

    DeviceConfigApplication: DataStore injected into Service

--- src/main/kotlin/de/igorakkerman/demo/deviceconfig/start/DeviceConfigApplication.kt
+++ src/main/kotlin/de/igorakkerman/demo/deviceconfig/start/DeviceConfigApplication.kt
@@ -1,10 +1,16 @@
 package de.igorakkerman.demo.deviceconfig.start
 
+import DataStore
+import DeviceService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
-@SpringBootApplication
-class DeviceConfigApplication
+@SpringBootApplication(scanBasePackages = ["de.igorakkerman.demo.deviceconfig"])
+class DeviceConfigApplication {
+    @Bean
+    fun deviceService(dataStore: DataStore) = DeviceService(dataStore)
+}
 
 fun main(commandLineArguments: Array<String>) {
     runApplication<DeviceConfigApplication>(*commandLineArguments)
