4f3673ef6065be4d7833243768b69159e16b7303
Igor Akkerman <commit@dynamite.software>
2021-02-19 20:58:24 +0100

    application.yaml: SQL arguments displayed

--- src/main/kotlin/de/igorakkerman/demo/deviceconfig/application/service.kt
+++ src/main/kotlin/de/igorakkerman/demo/deviceconfig/application/service.kt
@@ -1,9 +1,22 @@
-import de.igorakkerman.demo.deviceconfig.application.Device
-import de.igorakkerman.demo.deviceconfig.application.DeviceId
+package de.igorakkerman.demo.deviceconfig.application
 
 class DeviceService(private val dataStore: DataStore) {
-    fun createDevice(device: Device) = dataStore.createDevice(device)
-    fun findDeviceById(deviceId: DeviceId): Device? = dataStore.findDeviceById(deviceId)
-    fun updateDevice(device: Device) = dataStore.updateDevice(device)
-    fun findAllDevices(): List<Device> = dataStore.findAllDevices()
+    fun createDevice(device: Device) = dataStore.transactional {
+        dataStore.createDevice(device)
+    }
+
+    fun findDeviceById(deviceId: DeviceId): Device? = dataStore.transactional {
+        dataStore.findDeviceById(deviceId)
+    }
+
+    fun updateDevice(device: Device) = dataStore.transactional {
+        dataStore.updateDevice(device)
+    }
+
+    fun findAllDevices(): List<Device> = dataStore.transactional {
+        dataStore.findAllDevices()
+    }
 }
+
+class NoSuchItemException(deviceId: DeviceId) : RuntimeException(deviceId)
+class ItemAreadyExistsException(deviceId: DeviceId) : RuntimeException(deviceId)
