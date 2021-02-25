a37d8730fd2df793dfb637479f98b1d0f864a630
Igor Akkerman <commit@dynamite.software>
2021-02-25 15:29:52 +0100

    spring wiring optimized

--- src/test/kotlin/de/igorakkerman/demo/deviceconfig/api/rest/springmvc/BadRequestControllerTest.kt
+++ src/test/kotlin/de/igorakkerman/demo/deviceconfig/api/rest/springmvc/BadRequestControllerTest.kt
@@ -1,11 +1,7 @@
 package de.igorakkerman.demo.deviceconfig.api.rest.springmvc
 
-import de.igorakkerman.demo.deviceconfig.application.Computer
 import de.igorakkerman.demo.deviceconfig.application.DeviceService
-import de.igorakkerman.demo.deviceconfig.springboot.Application
-import de.igorakkerman.demo.deviceconfig.springboot.ServiceConfiguration
 import com.ninjasquad.springmockk.MockkBean
-import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -17,7 +13,7 @@ import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 
 @WebMvcTest(controllers = [DeviceController::class])
-@ContextConfiguration(classes = [Application::class, ServiceConfiguration::class])
+@ContextConfiguration(classes = [DeviceController::class])
 class BadRequestControllerTest(
     @Autowired
     private val mockMvc: MockMvc,
@@ -26,7 +22,6 @@ class BadRequestControllerTest(
     private lateinit var deviceService: DeviceService
 
     private val computerId = "macpro-m1-95014"
-    private val computer = Computer(computerId, "best mac", "timapple", "0n3m0r3th1ng", "192.168.178.1")
 
     @Test
     fun `request with wrong method should lead to reponse 405 method not allowed`() {
@@ -39,8 +34,6 @@ class BadRequestControllerTest(
 
     @Test
     fun `request with wrong 'accept' media type should lead to reponse 406 not acceptable`() {
-        every { deviceService.findDeviceById(computerId) } returns computer
-
         mockMvc.get("/devices/$computerId") {
             accept = APPLICATION_XML
         }.andExpect {
